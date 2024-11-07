588c62bedf52cff0e6ba91d8b4ae46b9cb3db0d6
DanhDepZai <danhhvph31168@fpt.edu.vn>
2024-11-07 09:00:07 +0700

    check task

--- app/Http/Controllers/Client/Product/ProductController.php
+++ app/Http/Controllers/Client/Product/ProductController.php
@@ -13,11 +13,11 @@ class ProductController extends Controller
 
     public function productDetail($slug)
     {
-        $product = Product::query()->with(['variants', 'category'])->where('slug', $slug)->first(); 
-        
+        $product = Product::query()->with(['variants', 'category'])->where('slug', $slug)->first();
+
         // Kiểm tra sản phẩm có tồn tại hay không
         if (!$product) {
-            abort(404); 
+            abort(404);
         }
           // Lấy 4 hình ảnh của sản phẩm galleries
         $product->galleries =  $product->galleries()->limit(4)->get();
@@ -28,6 +28,7 @@ class ProductController extends Controller
 
         // Lấy các bình luận cho sản phẩm
         $comments = Review::where('product_id', $product->id)->orderBy('id', 'DESC')->get();
+        
         // Lấy các sản phẩm liên quan dựa trên danh mục của sản phẩm hiện tại
         $relatedProducts = Product::with(['galleries', 'variants'])->where('category_id', $product->category_id)->where('id', '<>',$product->id)->limit(4)->get();
         return view('client.products.product-detail', compact('product', 'colors', 'sizes', 'comments', 'relatedProducts'));
