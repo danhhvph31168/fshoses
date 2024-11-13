9fcddce2b744edfa18816835cfd5d45081c5c1d4
NguyenDucHieu31723 <duchieu068@gmail.com>
2024-11-13 10:46:03 +0700

    Update order, orderItem

--- app/Http/Controllers/Account/OrderHistoryController.php
+++ app/Http/Controllers/Account/OrderHistoryController.php
@@ -4,25 +4,18 @@ namespace App\Http\Controllers\Account;
 
 use App\Http\Controllers\Controller;
 use App\Models\Order;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class OrderHistoryController extends Controller
 {
     public function getListOrderHistory()
     {
-        $orders = Auth::user()
-            ->orders()
-            ->with([
-                'orderItems.productVariant.product',
-                'orderItems.productVariant.size',
-                'orderItems.productVariant.color'
-            ])->orderBy('id', 'DESC')->paginate(5);
+        $orders = Auth::user()->orders()->orderBy('created_at', 'DESC')->paginate(5);
         return view("client.orders.list-order", compact("orders"));
     }
     public function getDetailOrderItem($slug)
     {
-        $order = Order::query()->where('sku_order',$slug)->with([
+        $order = Order::query()->where('sku_order', $slug)->with([
             "orderItems.productVariant.product",
             "orderItems.productVariant.color",
             "orderItems.productVariant.size",
@@ -30,7 +23,7 @@ class OrderHistoryController extends Controller
         if (!$order) {
             return view("page-error.404");
         }
-// dd  ($order);
+        // dd  ($order);
         return view("client.orders.detail-order", compact("order"));
     }
 }
